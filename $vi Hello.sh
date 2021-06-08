0126bae1d04b8374fb61f1e489dc9e4da22f95ef
charie15 <85291959+charie15@users.noreply.github.com>
2021-06-08 20:54:16 +0800

    Create $vi Hello.sh

---
+++ $vi Hello.sh
@@ -0,0 +1,10 @@
+#This is my first script
+clear
+echo "I am Cha."
+echo "I am 25 years old."
+echo "My full name is Charie Oreta."
+echo "From BSIT - BSIT401 A."
+echo "My motto in life is : Our greatest glory is not in never falling but in rising every time we fall."
+echo "_"
+echo "My goal is to graduate from this school."
+#echo "Hidden from timeline!"
